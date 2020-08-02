99ba182f52b82ba67be697855920a45e88d637c6
iskanderandrews <iskander.s.andrews@gmail.com>
2020-08-02 18:06:18 +0200

    Implementing variables on hacker script

--- 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
+++ 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    string greeting = "Hello Iskander!";
+
     // Start is called before the first frame update
     void Start()
     {
+        var generalGreeting = "Welcome at Terminal Hacker Game";
+        Terminal.WriteLine(generalGreeting);
         ShowMainMenu();
     }
 
     void ShowMainMenu()
     {
+        Terminal.WriteLine(greeting);
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local liberary");
         Terminal.WriteLine("Press 2 for the police station");
