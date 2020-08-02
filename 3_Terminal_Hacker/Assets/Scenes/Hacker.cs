0b0e2c705a7d3335752d368de2ded0477e1a490a
iskanderandrews <iskander.s.andrews@gmail.com>
2020-08-02 17:55:15 +0200

    Implementing the first function hacker script

--- 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
+++ 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
@@ -7,6 +7,11 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local liberary");
         Terminal.WriteLine("Press 2 for the police station");
