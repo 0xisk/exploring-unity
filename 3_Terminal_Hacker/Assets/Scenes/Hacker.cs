12ef26eefceb89895c8a66eaa4145ab639b324e6
iskanderandrews <iskander.s.andrews@gmail.com>
2020-08-02 17:38:16 +0200

    Editing Hacker script

--- 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
+++ 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
@@ -7,7 +7,8 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello Console!");
+        Terminal.WriteLine("Hello Terminal!");
     }
 
     // Update is called once per frame
