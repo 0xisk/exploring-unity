c6259b081c8b34f76bab8f95693d6b357953a9ad
iskanderandrews <iskander.s.andrews@gmail.com>
2020-08-02 17:47:29 +0200

    Implementing the welcome msg for the terminal game

--- 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
+++ 3_Terminal_Hacker/Assets/Scenes/Hacker.cs
@@ -7,8 +7,10 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello Console!");
-        Terminal.WriteLine("Hello Terminal!");
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local liberary");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter you selection: ");
     }
 
     // Update is called once per frame
